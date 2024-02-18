4f3c1f6f94f04e6ac9e27c53487a26c06e3faee2
Josemarie <153520174+Josemarieanto@users.noreply.github.com>
2024-02-18 19:17:39 +0530

    Add files via upload

---
+++ class.py
@@ -0,0 +1,93 @@
+### class cars:
+###     name : ""
+###     number : ""
+###     length : ""
+##
+###     def details(self):
+###         return self.name , "and the car number is" , self.number
+##    
+### obj1 = cars()
+### obj2 = cars()
+##
+##
+### obj1.name = "benz car"
+### obj1.number = 2345
+### obj1.length = 4751
+##
+### obj2.name = "maruti suzuki"
+### obj2.number = 4567
+### obj2.length = 3995
+##
+### print("Details of the cars")
+### print (obj1.name)
+### print(obj1.number)
+### print(obj1.length)
+### print()
+### print (obj2.name)
+### print(obj2.number)
+### print(obj2.length)
+##
+### class phone:
+###     name : ""
+###     number : ""
+###     price : ""
+##
+###     def details(self):
+###         return self.name , "and the phone number is" , self.number
+##    
+### obj1 = phone()
+### obj2 = phone()
+##
+##
+### obj1.name = "samsung"
+### obj1.number = 8765432110
+### obj1.price = 10000
+##
+### obj2.name = "redme"
+### obj2.number = 4567
+### obj2.price = 12000
+##
+### print("Details of the phone")
+### print (obj1.name)
+### print(obj1.number)
+### print(obj1.price)
+### print()
+### print (obj2.name)
+### print(obj2.number)
+### print(obj2.price)
+##
+##class computer:
+##    name : ""
+##    size : ""
+##    price : ""
+##
+##    def details(self):
+##        return self.name , "and the   is" , self.size
+##    
+##obj1 = computer()
+##obj2 = computer()
+##
+##
+##obj1.name = "acer"
+##obj1.size = 4047
+##obj1.price = 25000
+##
+##obj2.name = "dell"
+##obj2.size = 5057
+##obj2.price = 30000
+##
+##print("Details of the computer")
+##print (obj1.name)
+##print(obj1.size)
+##print(obj1.price)
+##print()
+##print (obj2.name)
+##print(obj2.size)
+##print(obj2.price)
+##
+##
+##
+##
+##
+##
+##
