fc6c6b4b431c4d20f47e557977a0cc5922037362
Josemarie <153520174+Josemarieanto@users.noreply.github.com>
2024-02-24 20:01:26 +0530

    class.py

--- class.py
+++ class.py
@@ -84,10 +84,58 @@
 ##print (obj2.name)
 ##print(obj2.size)
 ##print(obj2.price)
-##
-##
-##
-##
-##
-##
-##
+
+# class person:
+#     Name : ""
+#     Country : ""
+#     Date_of_Birth : ""
+
+#     def details(self):
+#         return self.Name , self.Country , self.Date_of_Birth
+    
+# current_year= 2024
+# def calculation(birthday_year):
+#     age = current_year - birthday_year
+#     return age
+    
+# obj1 = person()
+
+
+# obj1.Name = "Abhitabh Bachchan"
+# obj1.Country = "Maharashtra"
+# obj1.Date_of_Birth = "1962-11-12"
+
+# print("Person 1 :")
+# print ("Name: ",obj1.Name)
+# print("Country: ",obj1.Country)
+# print("Date of Birth: ",obj1.Date_of_Birth)
+# print("Age: ",calculation(1962))
+
+
+
+# class Bank:
+#     def account_creation(self,account_no,initial_deposit):
+#         print("New A/C No.: ",account_no ,"Deposit Amount: ",initial_deposit)
+#         print("Account created succesfully.\n")
+    
+#     def deposit(self,account_no,amount):
+#         print("Deposit Rs. ",amount," to A/C No. ",account_no)
+#         print("Deposit successful.")
+    
+#     def balance(self,account_no,bal_amount):
+#         print("A/C. No. ",account_no)
+#         print("Account balance: ", bal_amount)
+        
+# bank = Bank()
+# bank.account_creation("SB-123",1000)
+# bank.deposit("SB-123",600)
+# bank.balance("SB-123",1600)
+
+
+
+
+
+
+
+
+
